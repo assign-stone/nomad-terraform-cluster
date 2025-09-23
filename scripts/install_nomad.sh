55394e8219689a27640f3f10a7416a9a8562b1dd
Shivani Joshi <97306266+assign-stone@users.noreply.github.com>
2025-09-23 10:05:18 +0530

    Modify install_nomad.sh to add /opt/nomad directory
    Updated the installation script to create the /opt/nomad directory and set ownership.

--- scripts/install_nomad.sh
+++ scripts/install_nomad.sh
@@ -3,9 +3,14 @@ set -e
 
 NOMAD_VERSION="1.8.0"
 cd /tmp
+
+# download and install nomad binary
 curl -sSL "https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip" -o nomad.zip
 unzip -o nomad.zip
 install -m 0755 nomad /usr/local/bin/nomad
+
+# create user/dirs
 useradd --system --home /etc/nomad.d --shell /bin/false nomad || true
-mkdir -p /etc/nomad.d
+mkdir -p /etc/nomad.d /opt/nomad
+chown -R nomad:nomad /etc/nomad.d /opt/nomad
 chmod 700 /etc/nomad.d
