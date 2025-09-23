7d791b80546ec92e141f17f33e44fe772af440d4
Shivani Joshi <97306266+assign-stone@users.noreply.github.com>
2025-09-23 10:01:00 +0530

    Add nomad_version variable and update user_data
    Added nomad_version variable and updated user_data to include it.

--- modules/nomad-server/server.tf
+++ modules/nomad-server/server.tf
@@ -3,15 +3,21 @@ variable "ami_id" {}
 variable "instance_type" {}
 variable "security_group_ids" {}
 variable "key_name" {}
+variable "nomad_version" {}
 
 resource "aws_instance" "server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_group_ids
   key_name               = var.key_name
 
-  user_data = templatefile("${path.module}/../../scripts/bootstrap.sh", { role = "server" })
+  user_data = templatefile("${path.module}/../../scripts/bootstrap.sh", {
+    ROLE         = "server"
+    SERVER_IP    = ""
+    NOMAD_VERSION = var.nomad_version
+  })
+
 
   tags = {
     Name = "nomad-server"
@@ -20,4 +26,8 @@ resource "aws_instance" "server" {
 
 output "public_ip" {
   value = aws_instance.server.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.server.private_ip
 }
