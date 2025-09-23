340ee3f5f1e4791a7f49303dcbbe62f2899704bc
Shivani Joshi <97306266+assign-stone@users.noreply.github.com>
2025-09-23 10:06:11 +0530

    Enhance nomad_server and nomad_client modules
    Added nomad_version and server_private_ip to nomad_client module. Introduced output for nomad_server_private_ip.

--- main.tf
+++ main.tf
@@ -14,29 +14,36 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source       = "./modules/vpc"
   allowed_cidr = var.allowed_cidr
 }
 
 module "nomad_server" {
-  source          = "./modules/nomad-server"
-  subnet_id       = module.vpc.public_subnet_id
-  ami_id          = var.ami_id
-  instance_type   = var.server_instance_type
+  source             = "./modules/nomad-server"
+  subnet_id          = module.vpc.public_subnet_id
+  ami_id             = var.ami_id
+  instance_type      = var.server_instance_type
   security_group_ids = [module.vpc.nomad_sg_id]
-  key_name            = var.key_name
+  key_name           = var.key_name
+  nomad_version      = var.nomad_version
 }
 
 module "nomad_client" {
-  source          = "./modules/nomad-client"
-  subnet_id       = module.vpc.private_subnet_id
-  ami_id          = var.ami_id
-  instance_type   = var.client_instance_type
-  client_count    = var.client_count
+  source             = "./modules/nomad-client"
+  subnet_id          = module.vpc.private_subnet_id
+  ami_id             = var.ami_id
+  instance_type      = var.client_instance_type
+  client_count       = var.client_count
   security_group_ids = [module.vpc.nomad_sg_id]
-  key_name            = var.key_name
+  key_name           = var.key_name
+  server_private_ip  = module.nomad_server.private_ip
+  nomad_version      = var.nomad_version
 }
 
 output "nomad_server_public_ip" {
   value = module.nomad_server.public_ip
+}
+
+output "nomad_server_private_ip" {
+  value = module.nomad_server.private_ip
 }
